e0f562427a188a7eaac9666292cc155458cff832
Dima <diman-pichugin@yandex.ru>
2023-02-15 20:01:12 +0300

    init

--- src/main/java/com/pichugin/hello/Controller.java
+++ src/main/java/com/pichugin/hello/Controller.java
@@ -1,8 +1,9 @@
 package com.pichugin.hello;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@org.springframework.stereotype.Controller
+@RestController
 public class Controller {
 
     @GetMapping("/")
